564f4bc302ddf7377f81a6f9a16591f70bc6b7be
Natnael Samson <natnaelsamson41@gmail.com>
2025-01-22 10:54:13 +0300

    Added Mechanism To View Profile

---
+++ tinder-web-app/js/ViewProfile.js
@@ -0,0 +1,14 @@
+const userProfile = JSON.parse(localStorage.getItem('userProfile'));
+
+        if (userProfile) {
+            document.getElementById('profile-photo').src = userProfile.photo;
+            document.getElementById('profile-name').textContent = userProfile.name;
+            document.getElementById('profile-age').textContent = userProfile.age;
+            document.getElementById('profile-bio').textContent = userProfile.bio;
+            document.getElementById('profile-hobbies').textContent = userProfile.hobbies;
+            document.getElementById('profile-age-range').textContent = userProfile.ageRange;
+            document.getElementById('profile-distance').textContent = userProfile.distance;
+        } else {
+            alert("No profile found! Please create your profile first.");
+            window.location.href = "./profile-creation.html";
+        }
