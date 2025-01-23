771381a894cd07ccd092bab07d7459683d28ce13
Natnael Asfaw <149261198+Natthy2023@users.noreply.github.com>
2025-01-23 20:42:24 +0300

    Update script.js

--- tinder-web-app/js/script.js
+++ tinder-web-app/js/script.js
@@ -180,5 +180,5 @@ const userProfile = JSON.parse(localStorage.getItem('userProfile'));
             profile.src = userProfile.photo;
         } else {
             alert("No profile found! Please create your profile first.");
-            window.location.href = "../public/Profile-Creation.html";
+            window.location.href = "./Profile-Creation.html";
         }
